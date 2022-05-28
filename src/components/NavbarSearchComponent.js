c5c85a5aa264bd6014ecb5499dcef084a9864667
Swapneel Shubham <62759701+swapneel1026@users.noreply.github.com>
2022-05-28 18:48:43 +0530

    updated with search button

--- src/components/NavbarSearchComponent.js
+++ src/components/NavbarSearchComponent.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./NavbarSearchComponent.css";
 import SearchIcon from "@mui/icons-material/Search";
-import ClearIcon from "@mui/icons-material/Clear";
 import axios from "axios";
 import FoodSearchCard from "./FoodSearchCard";
 
@@ -11,18 +10,19 @@ const NavbarSearchComponent = () => {
   let API_KEY = "778a5d64c304d4a4840dd794583f49ec";
   let url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=77b47c11&app_key=${API_KEY}`;
 
-  const clearHandler = () => {
-    setReceipe([]);
-    setQuery("");
-  };
+  // const clearHandler = () => {
+  //   setReceipe([]);
+  //   setQuery("");
+  // };
 
-  const getUsers = async () => {
+  const getItems = async () => {
     const users = await axios.get(url);
     setReceipe(users.data.hits);
+    // console.log("hit");
   };
   useEffect(() => {
-    getUsers();
-  });
+    getItems();
+  }, []);
 
   return (
     <>
@@ -34,11 +34,7 @@ const NavbarSearchComponent = () => {
           onChange={(e) => setQuery(e.target.value)}
         />
 
-        {query.length !== 0 ? (
-          <ClearIcon className="searchIcon" onClick={clearHandler} />
-        ) : (
-          <SearchIcon className="searchIcon" />
-        )}
+        <SearchIcon className="searchIcon" onClick={getItems} />
       </div>
       <div className="itemcardContainer">
         {Receipe.map((foodItem, idx) => {
